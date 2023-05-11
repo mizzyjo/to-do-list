359ec89f9029fdb37abab53b098d2156237460b8
mizzyjo <jomizziie@gmail.com>
2023-05-11 18:27:36 +0900

    Make line-through when you check checkbox

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Item({id, item, handleOnDel}) {
+  const [line, setLine] = useState({ 
+    textDecoration: 'none' 
+  });
+  
+  const getCheckboxValue = (e) => {
+    console.log(e.target.checked);
+    if(e.target.checked){
+      setLine({...line, textDecoration: 'line-through'});
+      console.log(`line : ${line}`);
+    }else {
+      setLine({...line, textDecoration: 'none'});
+      console.log(`line : ${line}`);
+    }
+  }
+
   return (
+    // 체크박스의 checked가 true가 되면 
+    // 변수 line도 true가 되면서 
+    // style line-through를 적용
+    
       <li>
-        {item.text}
+        <input type='checkbox' onClick={getCheckboxValue}></input>
+        <span style={line}>{item.text}</span>
         <button onClick={() => {
           // console.log(id);
           handleOnDel(id)
