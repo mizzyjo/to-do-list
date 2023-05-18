1a3de4dd61d82e2ab153f9bebdeb58ae98c5a3c4
mizzyjo <jomizziie@gmail.com>
2023-05-18 13:37:46 +0900

    for myself 1

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -3,25 +3,56 @@ import {v4 as uuidv4} from 'uuid';
 export default function AddTodo({ onAdd }) {
 
   const [text, setText] = useState('');
-  const handleChange = (e) => setText(e.target.value);
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(text.trim().length === 0) {
-      return;
-    }
-    onAdd({id: uuidv4(), text, status: 'active'});
+    onAdd({ id : uuidv4(), text, status:'active'});
     setText('');
-  };
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input 
-        type='text' 
-        placeholder='Add Todo' 
-        value={text} 
-        onChange={handleChange} 
+        type="text"
+        value={text}
+        onChange={handleChange}
       />
-      <button>Add</button>
+      <button>ADD</button>
     </form>
+    
   );
+
+
+
+
+
+
+
+
+
+  // const [text, setText] = useState('');
+  // const handleChange = (e) => setText(e.target.value);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if(text.trim().length === 0) {
+  //     return;
+  //   }
+  //   onAdd({id: uuidv4(), text, status: 'active'});
+  //   setText('');
+  // };
+  // return (
+  //   <form onSubmit={handleSubmit}>
+  //     <input 
+  //       type='text' 
+  //       placeholder='Add Todo' 
+  //       value={text} 
+  //       onChange={handleChange} 
+  //     />
+  //     <button>Add</button>
+  //   </form>
+  // );
 }
 
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,43 +3,85 @@ import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
 export default function TodoList() {
-  const [todos, setTodos] = useState([
-    {id: '123', text:'공부하기', status: 'active'},
-    {id: '124', text:'운동하기', status: 'active'}
+  const [ todos, setTodos ] = useState([
+    {id: 123, text: '독서하기', status: 'active'},
+    {id: 124, text:'운동하기', status: 'active'},
   ]);
 
-  const handleAdd = (todo) => {
-    // 새로운 투두를 todos에 업데이트
-    console.log(todo);
-    // todos를 빙글빙글 돌면서 새로운 껍데기를 만들고 업데이트 된 todo만 뒤에 추가해줌
-    setTodos([ ...todos, todo]);
-  }
-
   const handleUpdate = (updated) => {
-    // 업데이트하고자 하는 것만 새로 대체해주고, 기존 아이템은 그대로 둔다.
-    setTodos(todos.map(t => t.id === updated.id ? updated : t));
+    setTodos(todos.map((todo) => {
+      return todo.id === updated.id ? updated : todo;
+    }));
   }
 
   const handleDelete = (deleted) => {
-    // 삭제하고자 하는 id가 아닌 것들만 모아서 set 해준다.
-    setTodos(todos.filter(t => t.id !== deleted.id));
+    setTodos(todos.filter((todo) => {
+      return todo.id !== deleted.id
+    }))
+  }
+
+  const handleAdd = (added) => {
+    console.log(todos);
+    setTodos([...todos, added])
   }
 
   return (
-    <section>
+    <>
       <ul>
         {
-          todos.map((item) => (
-          <Todo 
-            key={item.id} 
-            todo={item} 
-            onUpdate={handleUpdate} 
-            onDelete={handleDelete}/>
-          ))
+          todos.map((todo) => {
+            return <Todo todo={todo} onUpdate={handleUpdate} onDelete={handleDelete}>{todo.text}</Todo>
+          })
         }
       </ul>
       <AddTodo onAdd={handleAdd}/>
-    </section>
+    </>
   );
+
+
+
+
+
+
+
+
+  // const [todos, setTodos] = useState([
+  //   {id: '123', text:'공부하기', status: 'active'},
+  //   {id: '124', text:'운동하기', status: 'active'}
+  // ]);
+
+  // const handleAdd = (todo) => {
+  //   // 새로운 투두를 todos에 업데이트
+  //   console.log(todo);
+  //   // todos를 빙글빙글 돌면서 새로운 껍데기를 만들고 업데이트 된 todo만 뒤에 추가해줌
+  //   setTodos([ ...todos, todo]);
+  // }
+
+  // const handleUpdate = (updated) => {
+  //   // 업데이트하고자 하는 것만 새로 대체해주고, 기존 아이템은 그대로 둔다.
+  //   setTodos(todos.map(t => t.id === updated.id ? updated : t));
+  // }
+
+  // const handleDelete = (deleted) => {
+  //   // 삭제하고자 하는 id가 아닌 것들만 모아서 set 해준다.
+  //   setTodos(todos.filter(t => t.id !== deleted.id));
+  // }
+
+  // return (
+  //   <section>
+  //     <ul>
+  //       {
+  //         todos.map((item) => (
+  //         <Todo 
+  //           key={item.id} 
+  //           todo={item} 
+  //           onUpdate={handleUpdate} 
+  //           onDelete={handleDelete}/>
+  //         ))
+  //       }
+  //     </ul>
+  //     <AddTodo onAdd={handleAdd}/>
+  //   </section>
+  // );
 }
 
